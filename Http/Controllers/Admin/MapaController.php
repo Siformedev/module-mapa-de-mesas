64ef3705a9ae510496c48d35d33f1061ade5ccee
Leonardo Zanela <leonardozaneladias@gmail.com>
2020-07-17 18:13:30 -0300

    Criado CRUD de Mapas

---
+++ Http/Controllers/Admin/MapaController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Modules\MapaDeMesas\Http\Controllers\Admin;
+
+
+use DataTables\Editor\Format;
+use DataTables\Editor\Options;
+use DataTables\Editor\Validate;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller;
+
+use
+    DataTables\Database,
+    DataTables\Editor,
+    DataTables\Editor\Field;
+use Illuminate\Support\Facades\DB;
+
+class MapaController extends Controller
+{
+    public function index()
+    {
+        return view('mapademesas::admin.mapa.index');
+    }
+
+    public function datatable()
+    {
+        sleep(5);
+        $contract_id = filter_input(INPUT_GET, 'contract_id', FILTER_VALIDATE_INT);
+
+        Editor::inst( $this->db, 'mapas' )
+            ->fields(
+                Field::inst( 'mapas.id' ),
+                Field::inst( 'mapas.contract_id' )
+                    ->options( Options::inst()
+                        ->table( 'contracts' )
+                        ->value( 'id' )
+                        ->label( 'name' )
+                    )->Validator(Validate::notEmpty()),
+                Field::inst( 'mapas.event_id' )
+                    ->options( Options::inst()
+                        ->table( 'events' )
+                        ->value( 'id' )
+                        ->label( 'name' )
+                    )->Validator(Validate::notEmpty()),
+                Field::inst( 'mapas.nome' )
+                    ->Validator(Validate::notEmpty()),
+                Field::inst( 'mapas.data_inicio' )
+                    ->getFormatter( Format::dateSqlToFormat( 'd/m/Y H:i' ) )
+                    ->setFormatter( Format::dateTime( 'd/m/Y H:i', 'Y-m-d H:i:s' ) )
+                    ->Validator(Validate::notEmpty()),
+                Field::inst( 'mapas.data_fim' )
+                    ->getFormatter( Format::dateSqlToFormat( 'd/m/Y H:i' ) )
+                    ->setFormatter( Format::dateTime( 'd/m/Y H:i', 'Y-m-d H:i:s' ) )
+                    ->Validator(Validate::notEmpty()),
+                Field::inst( 'mapas.status' )
+                    ->Validator(Validate::boolean()),
+
+                /* LEFT JOIN  */
+                Field::inst( 'contracts.name' ),
+                Field::inst( 'events.name' )
+            )
+            ->where(function ($q) use ($contract_id){
+                if($contract_id){
+                    $q->where('mapas.contract_id', $contract_id);
+                }
+
+            })
+            ->leftJoin('contracts', 'contracts.id', '=', 'mapas.contract_id')
+            ->leftJoin('events', 'events.id', '=', 'mapas.event_id')
+            ->process( $_POST )
+            ->debug(true)
+            ->json();
+    }
+}
--- Http/routes.php
+++ Http/routes.php
@@ -1,6 +1,13 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'mapademesas', 'namespace' => 'Modules\MapaDeMesas\Http\Controllers'], function()
+Route::group(['middleware' => 'web', 'prefix' => 'mapademesas', 'as'=>'mapademesas.', 'namespace' => 'Modules\MapaDeMesas\Http\Controllers'], function()
 {
-    Route::get('/', 'MapaDeMesasController@index');
+    Route::get('/', 'MapaDeMesasController@index')->name('index');
+
+    Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'as'=>'admin.'], function()
+    {
+        Route::get('mapa', 'MapaController@index')->name('mapa.index');
+        Route::post('mapa/datatable', 'MapaController@datatable')->name('mapa.datatable');
+    });
+
 });
